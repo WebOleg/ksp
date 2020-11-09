48ccde25c5b67d9bf015e6f1c89088f7b16534fe
WebOleg <Olegminenko2005@gmail.com>
2020-11-10 01:33:05 +0200

    5 pages completed

---
+++ services-template.php
@@ -0,0 +1,165 @@
+<?php
+/*
+Template Name: Шаблон услуги
+*/
+?>
+
+<?php get_header(); ?>
+<section class="page-inner" style="background-image: url('<?php bloginfo('template_directory') ?>/img/services-inner.png')">
+  <div class="container">
+    <div class="page-info">
+      <h1>Наши услуги</h1>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna </p>
+    </div>
+  </div>
+</section>
+<section class="main-services">
+  <div class="container">
+    <h2 class="section-title">Основные услуги</h2>
+    <div class="services-row">
+      <div class="services-item main">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/s-liquidation.png" alt="Ликвидация предприятий"></div>
+        <h3 class="service-name">Ликвидация предприятий<br><i>(банкротство предприятий)</i></h3>
+        <p class="service-description">Квалифицированные адвокаты, юристы, специалисты Юридической фирмы «K&S partners» предоставляют полный комплекс юридических услуг и консультаций по вопросам ликвидации предприятий.</p>
+        <a href="" class="service-button">Подробнее</a>
+      </div>
+      <div class="services-item main">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/s-represent.png" alt="Представительство в судах"></div>
+        <h3 class="service-name">Представительство в судах</h3>
+        <p class="service-description">Представительство интересов в судах (представительство в суде) это непосредственное участие представителей Юридической фирмы «K&S partners» в местных судах общей юрисдикции, апелляционных и кассационных судах, а также в Международных коммерческих арбитражных судах и Европейском суде по правам человека.</p>
+        <a href="" class="service-button">Подробнее</a>
+      </div>
+      <div class="services-item main">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/s-abonent.png" alt="Абонентское юридическое обслуживание"></div>
+        <h3 class="service-name">АБОНЕНТСКОЕ<br>ЮРИДИЧЕСКОЕ ОБСЛУЖИВАНИЕ</h3>
+        <p class="service-description"> Данная услуга, актуальная при: проведении проверок контролирующими органами, заключении коммерческих договоров, сопровождении производственной деятельности, покупке бизнеса, реализации продукции, разработке внутренних положений в работе предприятия, либо существует необходимость привлечения квалифицированных специалистов в решении сложных вопросов.</p>
+        <a href="" class="service-button">Подробнее</a>
+      </div>
+      <div class="services-item main">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/s-debt.png" alt="ликвидация предприятий"></div>
+        <h3 class="service-name">Взыскание задолженности<br><i>(возврат долгов)</i></h3>
+        <p class="service-description">Квалифицированные адвокаты, юристы, специалисты Юридической фирмы «K&S partners» предоставляют полный комплекс юридических услуг и консультаций по вопросам взыскания задолженности</p>
+        <a href="" class="service-button">Подробнее</a>
+      </div>
+      <div class="services-item main">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/s-tax.png" alt="ликвидация предприятий"></div>
+        <h3 class="service-name">Налоговые споры</h3>
+        <p class="service-description">Квалифицированные адвокаты, юристы, специалисты Юридической фирмы «K&S partners» предоставляют полный комплекс юридических услуг и консультаций по вопросам налоговых правоотношений</p>
+        <a href="" class="service-button">Подробнее</a>
+      </div>
+      <div class="services-item main">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/s-own.png" alt="ликвидация предприятий"></div>
+        <h3 class="service-name">Признание<br>права собственности</h3>
+        <p class="service-description">Квалифицированные адвокаты, юристы, специалисты Юридической фирмы «K&S partners» предоставляют полный комплекс юридических услуг и консультаций по вопросам правоотношений с недвижимостью.</p>
+        <a href="" class="service-button">Подробнее</a>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="add-services">
+  <div class="container">
+    <h2 class="section-title">Дополнительные услуги</h2>
+    <div class="services-row">
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-01.png" alt="Услуги адвоката Днепр"></div>
+        <h3 class="service-name">Услуги адвоката Днепр</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-02.png" alt="Уголовные дела"></div>
+        <h3 class="service-name">Уголовные дела</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-03.png" alt="Адвокат по Польше Днепр"></div>
+        <h3 class="service-name">Адвокат по Польше Днепр</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-04.png" alt="Налоговое право"></div>
+        <h3 class="service-name">Налоговое право</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-05.png" alt="Инвестиции"></div>
+        <h3 class="service-name">Инвестиции</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-06.png" alt="Оформление наследства Днепр"></div>
+        <h3 class="service-name">Оформление наследства Днепр</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-07.png" alt="Гражданское и хозяйственное право"></div>
+        <h3 class="service-name">Гражданское<br>и хозяйственное право</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-08.png" alt="Земельное право"></div>
+        <h3 class="service-name">Земельное право</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-09.png" alt="Трудовое право"></div>
+        <h3 class="service-name">Трудовое право</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-10.png" alt="Уголовное право"></div>
+        <h3 class="service-name">Уголовное право</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-11.png" alt="Корпоративное право"></div>
+        <h3 class="service-name">Корпоративное право</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-12.png" alt="Жилищные споры"></div>
+        <h3 class="service-name">Жилищные споры</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-13.png" alt="Корпоративные споры"></div>
+        <h3 class="service-name">Корпоративные споры</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-14.png" alt="Ликвидация предприятий"></div>
+        <h3 class="service-name">Ликвидация предприятий<br>(банкротство предприятий)</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-15.png" alt="Наследственные споры"></div>
+        <h3 class="service-name">Наследственные споры</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-16.png" alt="Договорные споры"></div>
+        <h3 class="service-name">Договорные споры<br>(кредитные споры)</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-17.png" alt="Обжалование решений государственных органов"></div>
+        <h3 class="service-name">Обжалование решений<br>государственных органов</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-18.png" alt="Отвод земельных участков (отвод земли, землеотвод)"></div>
+        <h3 class="service-name">Отвод земельных участков<br>(отвод земли, землеотвод)</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-19.png" alt="cемейные споры"></div>
+        <h3 class="service-name">cемейные споры</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-20.png" alt="Трудовые споры"></div>
+        <h3 class="service-name">Трудовые споры</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-21.png" alt="Узаконивание самовольных строений Днепр"></div>
+        <h3 class="service-name">Узаконивание самовольных<br>строений Днепр</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-22.png" alt="Услуги юриста"></div>
+        <h3 class="service-name">Услуги юриста</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-23.png" alt="Юридическое 
+опровождение сделок"></div>
+        <h3 class="service-name">Юридическое<br>cопровождение сделок<br>(реорганизация, слияние, поглощение)</h3>
+      </div>
+      <div class="services-item add">
+        <div class="s-img"><img src="<?php bloginfo('template_directory') ?>/img/add-24.png" alt="Досудебное урегулирование споров"></div>
+        <h3 class="service-name">Досудебное<br>урегулирование споров</h3>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
